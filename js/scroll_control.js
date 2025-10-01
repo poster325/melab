5d270895f0ce8e2410160816ff96d824e4ca21d3
poster325 <kayn7325@gmail.com>
2025-10-02 07:26:38 +0900

    noscroll

--- js/scroll_control.js
+++ js/scroll_control.js
@@ -1,8 +1,5 @@
 var page = 2;
 
-var scroll_buffer = 0;
-const sec2_scroll_buffer_max = 5000;
-
 var scroll_ended = true;
 
 currentPage(2);
@@ -11,54 +8,19 @@ window.addEventListener("wheel", function(e) {
     if(scroll_ended == false) return;
     if(e.deltaY > 0) {
         if(page == 3) return;
-        if(page == 2){
-        if(scroll_buffer < sec2_scroll_buffer_max){
-            scroll_buffer += e.deltaY * 10;
-            //console.log(scroll_buffer);
-            changeImage(scroll_buffer, sec2_scroll_buffer_max);
-            return;
-        }
-        }
         page++;
-        if(page == 2) {
-        scroll_buffer = 0;
-        changeImage(0, sec2_scroll_buffer_max);
-        }
         if(page > 3) page = 3;
     }
     if(e.deltaY < 0) {
         if(page == 1) return;
-        if(page == 2){
-            if(scroll_buffer > 0){
-                scroll_buffer += e.deltaY * 10;
-                // console.log(scroll_buffer);
-                changeImage(scroll_buffer, sec2_scroll_buffer_max);
-                return;
-            }
-        }
         page--;
-        if(page == 2) {
-        scroll_buffer = sec2_scroll_buffer_max;
-        changeImage(sec2_scroll_buffer_max, sec2_scroll_buffer_max);
-        }
         if(page < 1) page = 1;
     }
     currentPage(page);
 })
 
 
-var earth_year = 1;
-var earth_image_storage = [];
-page = 2;
-
 document.addEventListener("DOMContentLoaded", function() {
-    for(let i=1; i<=251; i++){
-        earth_image_storage[i] = new Image();
-        earth_image_storage[i].src = "./assets/index/earth_animation/" + i.toString().padStart(4, '0') + ".png";
-    }
-
-    changeImage(0, sec2_scroll_buffer_max);
-
     window.scrollTo({
         top: (2 - 1) * window.innerHeight,
         left: 0,
@@ -111,24 +73,4 @@ function currentPage(idx) {
           document.getElementById("public_work").style.opacity = "100%";
         }
       }
-}
-
-function changeImage(idx, idx_max) {
-    imsi = parseInt(1 + 250 * (idx / idx_max));
-    if(imsi < 1) imsi = 1;
-    if(imsi > 251) imsi = 251;
-    if(imsi > earth_year){
-        for(let i=earth_year; i <= imsi; i++){
-        document.getElementById("earth").src = earth_image_storage[i].src;
-        if(i <= 151 + 15) document.getElementById('earth_date').innerHTML = "Historical, A.D. " + (1849 + i)
-        else document.getElementById('earth_date').innerHTML = "SSP585, A.D. " + (1849 + i)
-        }
-    } else {
-        for(let i=earth_year; i >= imsi; i--){
-        document.getElementById("earth").src = earth_image_storage[i].src;
-        if(i <= 151 + 15) document.getElementById('earth_date').innerHTML = "Historical, A.D. " + (1849 + i)
-        else document.getElementById('earth_date').innerHTML = "SSP585, A.D. " + (1849 + i)
-        }
-    }
-    earth_year = imsi;
 }
